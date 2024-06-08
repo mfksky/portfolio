078fb721ae97aeeabfa8ce28bef788299fffa0f1
Alex <63725893+mfksky@users.noreply.github.com>
2024-06-08 05:09:42 +0300

    Создал стандартный шаблон + перенес в него seo

--- src/utils/seo.js
+++ src/utils/seo.js
@@ -1,15 +1,20 @@
 import directus from './directus';
-import { readItems } from '@directus/sdk';
+import { readItems } from "@directus/sdk";
 
 export async function fetchSEOData(pageId) {
-	const seoData = await directus.request(
-		readItems('seo', {
-			filter: {
-				page_id: pageId,
-			},
-			fields: ['post_id', 'title', 'description'],
-		})
-	);
+	try {
+		const seoData = await directus.request(
+			readItems('seo', {
+				filter: {
+					page_id: pageId,
+				},
+				fields: ['post_id', 'title', 'description', 'lang', 'fonts'],
+			})
+		);
 
-	return seoData[0];
+		return seoData[0];
+	} catch (error) {
+		console.error('Error fetching SEO data:', error);
+		return null;
+	}
 }
