110a8a9fd52c44b94d3b47846bfe1756ea09a896
Alex <63725893+mfksky@users.noreply.github.com>
2024-06-14 05:52:25 +0300

    Main Form

--- src/utils/clientAuth.js
+++ src/utils/clientAuth.js
@@ -1,1 +1,8 @@
-export const DIRECTUS_URL = import.meta.env.DIRECTUS_URL;
+import { createDirectus, rest, readItem, createItem } from '@directus/sdk';
+
+const directus = createDirectus(import.meta.env.PUBLIC_DIRECTUS_URL)
+    .with(rest())
+    .with(readItem())
+    .with(createItem());
+
+export default directus;
